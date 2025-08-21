e8a86f4f5ecfc122b58bdff255d1f91068d58f86
Cristhian Alves <cristhian.ads@hotmail.com>
2025-08-20 22:55:27 -0300

    feat: aula02 - Comentários no JavaScript

---
+++ JavaScript Basico/Aula 02 - Comentarios/index.js
@@ -0,0 +1,16 @@
+// Escrevendo um comentário longo
+console.log('Olá mundo!')
+console.log('Linha 1') // Comentario simples
+/*
+* Um blodo de código
+* Linha de comentário.
+*/
+console.log('Linha 2')
+
+/*
+    console.log("Linha 3");
+    Não vai aparecer no engine
+*/
+
+console.log('Linha 4')
+// Vai ser ignorado
