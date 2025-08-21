a88101c084c06821afa217d761db391b19ad897c
Cristhian Alves <cristhian.ads@hotmail.com>
2025-08-20 23:05:20 -0300

    feat: aula07 - Manipulando dados primitivos

---
+++ JavaScript Basico/Aula 07 - Tipos de dados primitivos/index.js
@@ -0,0 +1,17 @@
+const nome = 'Cristhian' // String
+const nome1 = 'Alves' // String
+const nome2 = 'Maryana' // String
+const num1 = 10 // Number
+const num2 = 10.52 // Number
+let nomeAluno // Undefined -> não aponta para nenhum lugar na memória
+const sobrenomeAluno = null // Null -> não aponta para nenhum lugar na memória
+const aprovado = true // Boolean = true/false (lógico)
+
+console.log(typeof (nome))
+
+let a = 2
+const b = a
+console.log(a, b)
+
+a = 3
+console.log(a, b)
