d2bc4ba4341acb0c0ec493cc36aaeb2588b99514
Cristhian Alves <cristhian.ads@hotmail.com>
2025-08-20 23:09:10 -0300

    feat: aula09 - Manipulando alert, confirm e prompt no navegador

---
+++ JavaScript Basico/Aula 09 - alert, confirm e prompt (Navegador)/js/script.js
@@ -0,0 +1,7 @@
+let num1 = prompt('Digite um número')
+num1 = Number(num1)
+let num2 = prompt('Digite outro número')
+num2 = parseFloat(num2)
+
+const resultado = num1 + num2
+alert(`O resultado da sua conta foi: ${resultado}`)
