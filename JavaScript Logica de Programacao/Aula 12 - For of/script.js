b42e8a8fd3e687cb5af1b278d6990f913eeb2073
Cristhian Alves <cristhian.ads@hotmail.com>
2025-08-21 00:03:11 -0300

    feat: aula12- Estrutura de repetição For Of

---
+++ JavaScript Logica de Programacao/Aula 12 - For of/script.js
@@ -0,0 +1,10 @@
+const nome = ['Cristhian', 'Maryana', 'Maria', 'Cleiton'];
+
+for (let valor of nome) { // Usar quando precisar dos valores mesmo, sem ser indices ou chaves
+    console.log(valor);
+}
+
+
+// For Classico - Geralmente com iteraveis (array ou strings)
+// For in - Retorna o indice ou chave (strings, arrays ou objetos)
+// For of - Retorna o valor em si (iteraveis, array ou string)
