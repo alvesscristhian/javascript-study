e2f580adb62bc5761988cbe102441d03d54d9f5f
Cristhian Alves <cristhian.ads@hotmail.com>
2025-08-21 00:25:39 -0300

    feat: aula08 - Iteração sobre arrays com forEach

---
+++ JavaScript Array Avançado/Aula 08 - forEach/script.js
@@ -0,0 +1,6 @@
+// forEach((value, index, array) - Junção dos fors, itera sobre o array
+
+const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let total = 0;
+a1.forEach(value => total += value);
+console.log(total);
