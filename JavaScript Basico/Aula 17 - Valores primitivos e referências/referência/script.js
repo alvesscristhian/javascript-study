e59802d305904c6608faf2e27ecba57102f7aaf9
Cristhian Alves <cristhian.ads@hotmail.com>
2025-08-20 23:19:51 -0300

    feat: aula17 - Diferença entre valores Imutáveis e Mutável (Referência/Primitivo)

---
+++ JavaScript Basico/Aula 17 - Valores primitivos e referências/referência/script.js
@@ -0,0 +1,19 @@
+// // Primitivos (Imutáveis) - string, number, boolean, undefined, null (bigint, symbol)
+// let a = 'A';
+// let b = a; // Valores copiados
+// console.log(a, b);
+
+// // Referência (Mutável) - array, object, function
+// let a = [1, 2, 3];
+// let b = a; // Passados por referência
+// let b = [...a]; // Copia o valor de A para o B
+// console.log(a, b);
+
+// const a = {
+//     nome: 'Cristhian',
+//     sobrenome: 'Alves'
+// };
+// const b = a;
+
+// a.nome = 'Joao';
+// console.log(b);
