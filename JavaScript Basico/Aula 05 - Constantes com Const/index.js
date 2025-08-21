285b303a77227f67dc7b8b5ed00b39835ccf6b30
Cristhian Alves <cristhian.ads@hotmail.com>
2025-08-20 23:04:12 -0300

    feat: aula05 - Criando constantes com const

---
+++ JavaScript Basico/Aula 05 - Constantes com Const/index.js
@@ -0,0 +1,8 @@
+// String = Text | Number = Número
+const primeiroNumero = 5 // Number
+const segundoNumero = 10
+const resultado = primeiroNumero * segundoNumero
+const resultadoDuplicado = resultado * 2
+
+console.log('O resultado da multiplicação dos dois numeros é:', resultadoDuplicado)
+console.log(typeof (primeiroNumero))
