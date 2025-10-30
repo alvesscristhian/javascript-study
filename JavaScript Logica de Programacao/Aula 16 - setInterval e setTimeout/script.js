04cce28b3412c1acc8f07898a5cf9d916ef881ac
Cristhian Alves <cristhian.ads@hotmail.com>
2025-10-29 23:22:37 -0300

    Aprendendo a controlar tempos entre sistemas com setInterval e setTimeout

---
+++ JavaScript Logica de Programacao/Aula 16 - setInterval e setTimeout/script.js
@@ -0,0 +1,12 @@
+function mostraHora() {
+    let data = new Date();
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false
+    });
+}
+
+const timer = setInterval(function () {
+    console.log(mostraHora());
+}, 1000); // 1000ms equivale a 1s
+
