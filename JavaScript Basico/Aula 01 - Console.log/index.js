9674c3bbde063495f66ca48536be256c7dca43d5
Cristhian Alves <cristhian.ads@hotmail.com>
2025-10-29 22:47:39 -0300

    Aprendendo a usar Console.log

---
+++ JavaScript Basico/Aula 01 - Console.log/index.js
@@ -0,0 +1,5 @@
+console.log("'Cristhian Alves'") // String = Texto
+console.log('"Cristhian Alves"') // String = Texto
+console.log('\'Cristhian\' "Alves"') // String = Texto
+
+console.log(123456, 15.85) // Number = Número
