0db68cf821f958505a7e4bbb124d7b5ddec5e1c9
Cristhian Alves <cristhian.ads@hotmail.com>
2025-10-30 00:00:06 -0300

    Aprendendo a copiar um array e manusear seus avlores

---
+++ JavaScript Array Avançado/Aula 02 - Método Splice/script.js
@@ -0,0 +1,7 @@
+const nomes = ['Cristhian', 'Maryana', 'Marcela', 'Max', 'Matheus'];
+
+// nomes.splice(indice, qtd.delete, add.element1, add.element2, add.element3);
+
+const removidos = nomes.splice(-1, 1);
+
+console.log(nomes, removidos);
