866e87c05718e61ab3ea76f79a99f4c7eb3d29a3
Cristhian Alves <cristhian.ads@hotmail.com>
2025-10-29 23:06:01 -0300

    Aprendendo a utilizar operadores lógicos

---
+++ JavaScript Logica de Programacao/Aula 02 - Operadores Logicos/index.js
@@ -0,0 +1,11 @@
+const expressaoAnd = true && true && false && true;
+const expressaoOr = true || false;
+
+
+const usuario = 'Cristhian';
+const senha = '123456';
+const validar = usuario === 'Cristhian' && senha === '123456';
+console.log(validar);
+
+
+console.log(!true);
