e9d68312ff1f9eb0961a198908819e69472b44da
Cristhian Alves <cristhian.ads@hotmail.com>
2025-10-29 22:55:24 -0300

    Aprendendo a utilizar tipos de operadores e atribuir valores a variaveis

---
+++ JavaScript Basico/Aula 08 - Operadores aritméticos, de atribuição e incrementos/index.js
@@ -0,0 +1,24 @@
+/* Aritméticos
+
++ -> Adição / Concatenação
+- -> Subtração
+/ -> Divisão
+* -> Multiplicacão
+** -> Potenciação
+% -> Resto da divisão
+++ -> Incremento
+-- -> Decremento
++=, -=, *= ou /= -> Operadores de atribuição
+
+Precedência
+1º ()
+2º **
+3º * / %
+4º + -
+
+*/
+
+const num1 = 10
+const num2 = Number('5.2')
+console.log(num1 + num2)
+console.log(typeof num2)
