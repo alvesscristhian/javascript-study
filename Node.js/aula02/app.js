fb23cd140727cf6e7c1345d240b4e9d649df80f0
Cristhian Alves <cristhian.ads@hotmail.com>
2025-10-30 00:41:09 -0300

    NPM

---
+++ Node.js/aula02/app.js
@@ -0,0 +1,8 @@
+const path = require('path');
+console.log(__dirname);
+console.log(path.resolve(__dirname, '..', '.', 'arquivos', 'img'));
+
+
+// const  Cachorro = require('./mod');
+// const cachorrinho = new Cachorro('Docky');
+// // cachorrinho.latir();
