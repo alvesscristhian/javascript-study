8dcf27b328ba664c7b6665da2e0f23a900d8b237
Cristhian Alves <cristhian.ads@hotmail.com>
2025-11-29 23:26:23 -0300

    feat: Validando o formulário de agendamento

--- Node.js/projetoAgenda/frontend/main.js
+++ Node.js/projetoAgenda/frontend/main.js
@@ -3,8 +3,11 @@ import 'regenerator-runtime/runtime';
 
 import Cadastro from './modules/Cadastro'
 import Login from './modules/Login';
+import Agendamento from './modules/Agendamento';
 
 const login = new Login('.form-login');
 const cadastro = new Cadastro('.form-cadastro');
+const agendamento = new Agendamento('.form-agendamento');
 login.init();
 cadastro.init();
+agendamento.init();
