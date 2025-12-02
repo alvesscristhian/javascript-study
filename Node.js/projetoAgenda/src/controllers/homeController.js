ddb2f644cb264414f38d9f7133d80e18b4825d79
Cristhian Alves <cristhian.ads@hotmail.com>
2025-12-02 10:49:27 -0300

    feat: Estilizando as páginas de agendamento

--- Node.js/projetoAgenda/src/controllers/homeController.js
+++ Node.js/projetoAgenda/src/controllers/homeController.js
@@ -1,7 +1,13 @@
 const Contato = require('../models/ContatoModel');
 
 exports.index = async (req, res) => {
-    const agendamentos = await Contato.buscaClientes();
-    res.render('index', { agendamentos });
-    return;
+    try {
+        const agendamentos = await Contato.buscaClientes();
+        const user = req.session.user;
+        res.render('index', { agendamentos, user });
+        return;
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    }
 };
