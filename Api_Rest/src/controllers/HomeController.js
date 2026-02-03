f400c682f800f4d5f6c8f68ab0a10e820d66055e
Cristhian Alves <cristhian.ads@hotmail.com>
2026-02-03 00:46:38 -0300

    feat: Configurando o Sequelize

--- Api_Rest/src/controllers/HomeController.js
+++ Api_Rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Cristhian',
+      sobrenome: 'Alves',
+      email: 'cristhian.ads@hotmail.com',
+      idade: 18,
+      peso: 68,
+      altura: 1.70,
     });
+    res.json(novoAluno);
   }
 }
 
