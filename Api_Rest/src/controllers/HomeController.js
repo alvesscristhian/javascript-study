7ad536acb053a9e073a9990f5bb4eb66d0ec32ad
Cristhian Alves <cristhian.ads@hotmail.com>
2026-02-27 11:24:02 -0300

    feat: Criando rotas de alunos

--- Api_Rest/src/controllers/HomeController.js
+++ Api_Rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Cristhian',
-      sobrenome: 'Alves',
-      email: 'cristhian.ads@hotmail.com',
-      idade: 18,
-      peso: 68,
-      altura: 1.70,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
---
+++ Api_Rest/src/database/migrations/20260227141325-mudar-email-aluno-unique.js
@@ -0,0 +1,15 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  // changeColumn => Alterar uma coluna já existente
+  up: (queryInterface, Sequelize) => queryInterface.changeColumn(
+    'alunos',
+    'email',
+    {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  ),
+
+  down: () => {},
+};
