8c38304cc4aed54534cc1ae149a81ec98e2ef9be
Cristhian Alves <cristhian.ads@hotmail.com>
2026-03-23 13:40:29 -0300

    Salvando uploads de arquivos

---
+++ Api_Rest/src/database/migrations/20260323162737-criar-tabela-de-foto-do-aluno.js
@@ -0,0 +1,43 @@
+// Migrações: Servem para gerenciar e versionar a estrutura do banco de dados
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('fotos', { // Cria a tabela no banco de dados
+      id: {
+        type: Sequelize.INTEGER, // Define o tipo: String/Integer/Char/Float/Date/Boolean
+        allowNull: false, // Permissão para deixar esse campo nulo?
+        autoIncrement: true, // Permissão para auto incrementar?
+        primaryKey: true, // Chave primaria?
+      },
+      originalname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      aluno_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'alunos', // Referência a tabela alunos pela chave id
+          key: 'id',
+        },
+        onDelete: 'SET NULL',
+        onUpdate: '',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('alunos');
+  },
+};
